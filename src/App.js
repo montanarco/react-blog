0887072046d7870e45ddfad8f38a6200d4a8b69a
Miguel Montanez1 <miguel_montanez1@epam.com>
2025-01-28 18:52:37 -0500

    nav bar enhancement

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import './App.css';
+import NavBar from './NavBar';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlesList  from './pages/ArticlesList';
+import ArticlesPage from './pages/ArticlePage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+      <NavBar></NavBar>
+      <h1> Simple Blog </h1>
+      <div id="page-body">
+          <Routes>
+            <Route path="/" element={<HomePage/>}/>
+            <Route path="/about" element={<AboutPage/>}/>
+            <Route path="/articles" element={<ArticlesList/>}/>
+            <Route path="/articles/:articleId" element={<ArticlesPage/>}/>
+          </Routes>
+      </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/ArticlePage.js
@@ -0,0 +1,7 @@
+const ArticlesPage = () =>{
+    return(
+        <h1> This is the ArticlesPage Page!</h1>
+    );
+}
+
+export default ArticlesPage;
